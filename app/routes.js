eece121f4fc12d73a5beefc7360345c54780a1e9
Simon Whatley <simonwhatley@gmail.com>
2023-12-11 15:44:23 +0000

    Add routes

--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,118 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+
+const settings = require('./data/settings')
+
+/// ------------------------------------------------------------------------ ///
+/// Flash messaging
+/// ------------------------------------------------------------------------ ///
+const flash = require('connect-flash')
+router.use(flash())
+
+/// ------------------------------------------------------------------------ ///
+/// User authentication
+/// ------------------------------------------------------------------------ ///
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const authenticationModel = require('./models/authentication')
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+// Authentication
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    const user = authenticationModel.findOne({
+      username: username,
+      password: password,
+      active: true
+    })
+    if (user) { return done(null, user) }
+    return done(null, false)
+  }
+))
+
+router.use(passport.initialize())
+router.use(passport.session())
+
+// Controller modules
+const accountController = require('./controllers/account')
+const authenticationController = require('./controllers/authentication')
+
+// Authentication middleware
+const checkIsAuthenticated = (req, res, next) => {
+  if (req.session.passport) {
+    // the signed in user
+    res.locals.passport = req.session.passport
+    // the base URL for navigation
+    // res.locals.baseUrl = `/organisations/${req.params.organisationId}`
+    next()
+  } else {
+    delete req.session.data
+    res.redirect('/sign-in')
+  }
+}
+
+/// ------------------------------------------------------------------------ ///
+/// ALL ROUTES
+/// ------------------------------------------------------------------------ ///
+
+router.all('*', (req, res, next) => {
+  res.locals.settings = settings
+  res.locals.referrer = req.query.referrer
+  res.locals.query = req.query
+  res.locals.flash = req.flash('success') // pass through 'success' messages only
+  next()
+})
+
+/// ------------------------------------------------------------------------ ///
+/// AUTHENTICATION ROUTES
+/// ------------------------------------------------------------------------ ///
+
+router.get('/sign-in', authenticationController.sign_in_get)
+router.post('/sign-in', passport.authenticate('local', {
+  successRedirect: '/auth',
+  failureRedirect: '/sign-in',
+  failureFlash: 'Enter valid sign-in details'
+}))
+
+router.get('/auth', authenticationController.auth_get)
+
+router.get('/sign-out', authenticationController.sign_out_get)
+
+router.get('/register', authenticationController.register_get)
+router.post('/register', authenticationController.register_post)
+
+router.get('/confirm-email', authenticationController.confirm_email_get)
+router.post('/confirm-email', authenticationController.confirm_email_post)
+
+router.get('/resend-code', authenticationController.resend_code_get)
+router.post('/resend-code', authenticationController.resend_code_post)
+
+router.get('/forgotten-password', authenticationController.forgotten_password_get)
+router.post('/forgotten-password', authenticationController.forgotten_password_post)
+
+router.get('/verification-code', authenticationController.verification_code_get)
+router.post('/verification-code', authenticationController.verification_code_post)
+
+router.get('/create-password', authenticationController.create_password_get)
+router.post('/create-password', authenticationController.create_password_post)
+
+router.get('/password-reset', authenticationController.password_reset_get)
+router.post('/password-reset', authenticationController.password_reset_post)
+
+router.get('/registration-complete', authenticationController.registration_complete_get)
+
+router.get('/terms-and-conditions', authenticationController.terms_and_conditions_get)
+
+/// ------------------------------------------------------------------------ ///
+/// YOUR ACCOUNT ROUTES
+/// ------------------------------------------------------------------------ ///
+
+router.get('/account', checkIsAuthenticated, accountController.user_account)
