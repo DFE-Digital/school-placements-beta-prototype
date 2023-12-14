a8e03ca1686bf4d87e76eee13ccdcf41861125b2
Simon Whatley <simonwhatley@gmail.com>
2023-12-14 14:39:14 +0000

    School data for lookups

---
+++ app/models/schools.js
@@ -0,0 +1,14 @@
+exports.findMany = (params) => {
+  let schools = require('../data/schools/schools.json')
+
+  if (params.query?.length) {
+    const query = params.query.toLowerCase()
+    return schools.filter(school =>
+      school.name.toLowerCase().includes(query)
+      || school.urn?.toString().includes(query)
+      || school.address?.postcode?.toLowerCase().includes(query)
+     )
+  }
+
+  return schools
+}
