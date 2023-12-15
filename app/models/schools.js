ff6f04d8ad97d7fc74b5837d58d89f07cc9b8b3f
Simon Whatley <simonwhatley@gmail.com>
2023-12-15 16:24:15 +0000

    Add findOne method to school model

--- app/models/schools.js
+++ app/models/schools.js
@@ -12,3 +12,18 @@ exports.findMany = (params) => {
 
   return schools
 }
+
+exports.findOne = (params) => {
+  let schools = require('../data/schools/schools.json')
+
+  if (params.query?.length) {
+    const query = params.query.toLowerCase()
+    return schools.find(school =>
+      school.name.toLowerCase().includes(query)
+      || school.urn?.toString().includes(query)
+      || school.address?.postcode?.toLowerCase().includes(query)
+     )
+  } else {
+    return null
+  }
+}
