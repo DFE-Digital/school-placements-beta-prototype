ec2718f675a556f8e4bf47d38cd4ae44d5e49b9c
Simon Whatley <simonwhatley@gmail.com>
2023-12-15 11:50:22 +0000

    Add GIAS filters

--- app/filters.js
+++ app/filters.js
@@ -1,6 +1,8 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
+const giasHelper = require('./helpers/gias')
+
 /* ------------------------------------------------------------------
 utility function to get an error for a component
 example: {{ errors | getErrorMessage('title') }}
@@ -17,3 +19,32 @@ addFilter('getErrorMessage', (array, fieldName) => {
 
   return error
 })
+
+/* ------------------------------------------------------------------
+GIAS utility functions
+------------------------------------------------------------------ */
+addFilter('getEstablishmentTypeLabel', giasHelper.getEstablishmentTypeLabel)
+
+addFilter('getEstablishmentGroupLabel', giasHelper.getEstablishmentGroupLabel)
+
+addFilter('getEstablishmentStatusLabel', giasHelper.getEstablishmentStatusLabel)
+
+addFilter('getEstablishmentPhaseLabel', giasHelper.getEstablishmentPhaseLabel)
+
+addFilter('getGenderLabel', giasHelper.getGenderLabel)
+
+addFilter('getOfficialSixthFormLabel', giasHelper.getOfficialSixthFormLabel)
+
+addFilter('getNurseryProvisionLabel', giasHelper.getNurseryProvisionLabel)
+
+addFilter('getReligiousCharacterLabel', giasHelper.getReligiousCharacterLabel)
+
+addFilter('getAdmissionsPolicyLabel', giasHelper.getAdmissionsPolicyLabel)
+
+addFilter('getSpecialClassesLabel', giasHelper.getSpecialClassesLabel)
+
+addFilter('getUrbanRuralLabel', giasHelper.getUrbanRuralLabel)
+
+addFilter('getOfstedRatingLabel', giasHelper.getOfstedRatingLabel)
+
+addFilter('getSENDProvisionLabel', giasHelper.getSENDProvisionLabel)
