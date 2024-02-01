05954cc2bb8caca671594080b396d83ef18b6303
Simon Whatley <simonwhatley@gmail.com>
2024-02-01 16:48:55 +0000

    Update controllers

--- app/controllers/support/organisation-users.js
+++ app/controllers/support/organisation-users.js
@@ -37,7 +37,7 @@ exports.user_details = (req, res) => {
 
   const signedInUser = userModel.findOne({ userId: req.session.passport.user.id })
 
-  res.render('../views/support/organisations/users/details', {
+  res.render('../views/support/organisations/users/show', {
     organisation,
     user,
     signedInUser,
