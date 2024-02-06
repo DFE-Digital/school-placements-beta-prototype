484f0d68f089c9f814bc5f8b2b5912da8a450a21
Simon Whatley <simonwhatley@gmail.com>
2024-02-06 16:17:30 +0000

    Add support organisation mentor controller

---
+++ app/controllers/support/organisation-mentors.js
@@ -0,0 +1,170 @@
+const mentorModel = require('../../models/mentors')
+const organisationModel = require('../../models/organisations')
+const teacherModel = require('../../models/teachers')
+const validationHelper = require('../../helpers/validators')
+
+/// ------------------------------------------------------------------------ ///
+/// LIST MENTOR
+/// ------------------------------------------------------------------------ ///
+
+exports.mentor_list = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  const mentors = mentorModel.findMany({ organisationId: req.params.organisationId })
+
+  delete req.session.data.mentor
+
+  res.render('../views/support/organisations/mentors/list', {
+    organisation,
+    mentors,
+    actions: {
+      new: `/support/organisations/${req.params.organisationId}/mentors/new`,
+      view: `/support/organisations/${req.params.organisationId}/mentors`
+    }
+  })
+}
+
+/// ------------------------------------------------------------------------ ///
+/// SHOW MENTOR
+/// ------------------------------------------------------------------------ ///
+
+exports.mentor_details = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  const mentor = mentorModel.findOne({ mentorId: req.params.mentorId })
+
+  res.render('../views/support/organisations/mentors/show', {
+    organisation,
+    mentor,
+    actions: {
+      change: `/support/organisations/${req.params.organisationId}/mentors/${req.params.mentorId}/edit?referrer=change`,
+      delete: `/support/organisations/${req.params.organisationId}/mentors/${req.params.mentorId}/delete`,
+      back: `/support/organisations/${req.params.organisationId}/mentors`
+    }
+  })
+}
+
+/// ------------------------------------------------------------------------ ///
+/// NEW MENTOR
+/// ------------------------------------------------------------------------ ///
+
+exports.new_mentor_get = (req, res) => {
+  let back = `/support/organisations/${req.params.organisationId}/mentors`
+  if (req.query.referrer === 'check') {
+    back = `/support/organisations/${req.params.organisationId}/mentors/new/check`
+  }
+
+  res.render('../views/support/organisations/mentors/find', {
+    mentor: req.session.data.mentor,
+    actions: {
+      save: `/support/organisations/${req.params.organisationId}/mentors/new`,
+      back,
+      cancel: `/support/organisations/${req.params.organisationId}/mentors`
+    }
+  })
+}
+
+exports.new_mentor_post = (req, res) => {
+  const errors = []
+
+  if (!req.session.data.mentor.trn.length) {
+    const error = {}
+    error.fieldName = 'mentor'
+    error.href = '#mentor'
+    error.text = 'Enter a teacher reference number (TRN)'
+    errors.push(error)
+  } else if (!validationHelper.isValidTRN(req.session.data.mentor.trn)) {
+    const error = {}
+    error.fieldName = 'mentor'
+    error.href = '#mentor'
+    error.text = 'Enter a valid teacher reference number (TRN)'
+    errors.push(error)
+  }
+
+  const mentor = mentorModel.findOne({
+    organisationId: req.params.organisationId,
+    trn: req.session.data.mentor.trn
+  })
+
+  if (mentor) {
+    const error = {}
+    error.fieldName = 'mentor'
+    error.href = '#mentor'
+    error.text = 'Mentor has already been added'
+    errors.push(error)
+  }
+
+  if (errors.length) {
+    res.render('../views/support/organisations/mentors/find', {
+      mentor: req.session.data.mentor,
+      actions: {
+        save: `/support/organisations/${req.params.organisationId}/mentors/new`,
+        back: `/support/organisations/${req.params.organisationId}/mentors`,
+        cancel: `/support/organisations/${req.params.organisationId}/mentors`
+      },
+      errors
+    })
+  } else {
+    res.redirect(`/support/organisations/${req.params.organisationId}/mentors/new/check`)
+  }
+}
+
+exports.new_mentor_check_get = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  const mentor = teacherModel.findOne({ query: req.session.data.mentor.trn })
+
+  res.render('../views/support/organisations/mentors/check-your-answers', {
+    organisation,
+    mentor,
+    actions: {
+      save: `/support/organisations/${req.params.organisationId}/mentors/new/check`,
+      back: `/support/organisations/${req.params.organisationId}/mentors/new`,
+      change: `/support/organisations/${req.params.organisationId}/mentors/new?referrer=check`,
+      cancel: `/support/organisations/${req.params.organisationId}/mentors`
+    }
+  })
+}
+
+exports.new_mentor_check_post = (req, res) => {
+  const mentor = teacherModel.findOne({ query: req.session.data.mentor.trn })
+
+  mentorModel.saveOne({
+    organisationId: req.params.organisationId,
+    mentor
+  })
+
+  delete req.session.data.mentor
+
+  req.flash('success', 'Mentor added')
+  res.redirect(`/support/organisations/${req.params.organisationId}/mentors`)
+}
+
+/// ------------------------------------------------------------------------ ///
+/// DELETE MENTOR
+/// ------------------------------------------------------------------------ ///
+
+exports.delete_mentor_get = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  const mentor = mentorModel.findOne({
+    organisationId: req.params.organisationId,
+    mentorId: req.params.mentorId
+  })
+
+  res.render('../views/support/organisations/mentors/delete', {
+    organisation,
+    mentor,
+    actions: {
+      save: `/support/organisations/${req.params.organisationId}/mentors/${req.params.mentorId}/delete`,
+      back: `/support/organisations/${req.params.organisationId}/mentors/${req.params.mentorId}`,
+      cancel: `/support/organisations/${req.params.organisationId}/mentors/${req.params.mentorId}`
+    }
+  })
+}
+
+exports.delete_mentor_post = (req, res) => {
+  mentorModel.deleteOne({
+    organisationId: req.params.organisationId,
+    mentorId: req.params.mentorId
+  })
+
+  req.flash('success', 'Mentor removed')
+  res.redirect(`/support/organisations/${req.params.organisationId}/mentors`)
+}
