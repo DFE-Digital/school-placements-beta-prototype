13c11ec49ca3a156bfccb7c588b8a9ca3a0b6704
Zoe <zoefflanagan@gmail.com>
2024-02-21 14:47:30 +0000

    Update to the check answers page

--- app/controllers/placements.js
+++ app/controllers/placements.js
@@ -1,6 +1,7 @@
 const placementModel = require('../models/placements')
 const organisationModel = require('../models/organisations')
 const mentorHelper = require('../helpers/mentors')
+const subjectHelper = require('../helpers/subjects')
 
 /// ------------------------------------------------------------------------ ///
 /// LIST PLACEMENT
@@ -9,9 +10,9 @@ const mentorHelper = require('../helpers/mentors')
 exports.placement_list = (req, res) => {
     const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
     const placements = placementModel.findMany({ organisationId: req.params.organisationId })
-  
+
     delete req.session.data.placement
-  
+
     res.render('../views/placements/list', {
         organisation,
         placements,
@@ -29,14 +30,31 @@ exports.placement_list = (req, res) => {
 /// ------------------------------------------------------------------------ ///
 
 exports.new_placement_get = (req, res) => {
-
-    res.render('../views/placements/phase', {
+    const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+    if (!req.session.data.placement) {
+        req.session.data.placement = {}
+      }
+    if ([2,3].includes(organisation.establishmentPhase)) {
+    req.session.data.placement.subjectLevel = 'primary'
+    // redirect to primary subjects
+    res.redirect(`/organisations/${req.params.organisationId}/placements/new/subject`)
+    } else if ([4,5].includes(organisation.establishmentPhase)) {
+    req.session.data.placement.subjectLevel = 'secondary'
+    // redirect to secondary subjects
+    res.redirect(`/organisations/${req.params.organisationId}/placements/new/subject`)
+    } else {
+    // show subject level form
+    const subjectLevelOptions = subjectHelper.getSubjectLevelOptions(req.session.data.placement.subjectLevel)
+    res.render('../views/placements/subject-level', {
+        organisation,
         placement: req.session.data.placement,
+        subjectLevelOptions,
         actions: {
           save: `/organisations/${req.params.organisationId}/placements/new`,
           back: `/organisations/${req.params.organisationId}/placements`
         }
       })
+    }
 }
 
 exports.new_placement_post = (req, res) => {
@@ -44,9 +62,12 @@ exports.new_placement_post = (req, res) => {
 }
 
 exports.new_placement_subject_get = (req, res) => {
-
+    const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+    const subjectOptions = subjectHelper.getSubjectOptions(req.session.data.placement.subjectLevel)
     res.render('../views/placements/subject', {
+        organisation,
         placement: req.session.data.placement,
+        subjectOptions,
         actions: {
           save: `/organisations/${req.params.organisationId}/placements/new/subject`,
           back: `/organisations/${req.params.organisationId}/placements/new`
@@ -59,8 +80,10 @@ exports.new_placement_subject_post = (req, res) => {
 }
 
 exports.new_placement_mentor_get = (req, res) => {
+    const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
     const mentorOptions = mentorHelper.getMentorOptions({ organisationId: req.params.organisationId })
     res.render('../views/placements/mentor', {
+        organisation,
         placement: req.session.data.placement,
         mentorOptions,
         actions: {
@@ -75,8 +98,9 @@ exports.new_placement_mentor_post = (req, res) => {
 }
 
 exports.new_placement_window_get = (req, res) => {
-
+    const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
     res.render('../views/placements/window', {
+        organisation,
         placement: req.session.data.placement,
         actions: {
           save: `/organisations/${req.params.organisationId}/placements/new/window`,
@@ -90,16 +114,29 @@ exports.new_placement_window_post = (req, res) => {
 }
 
 exports.new_placement_check_get = (req, res) => {
+    const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
 
     res.render('../views/placements/check', {
+        organisation,
         placement: req.session.data.placement,
         actions: {
-          save: `/organisations/${req.params.organisationId}/placements/new`,
-          back: `/organisations/${req.params.organisationId}/placements/new/window`
+          save: `/organisations/${req.params.organisationId}/placements/new/check`,
+          back: `/organisations/${req.params.organisationId}/placements/new/window`,
+          change: `/organisations/${req.params.organisationId}/mentors/new?referrer=check`
         }
       })
 }
 
 exports.new_placement_check_post = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
 
+  placementModel.saveOne({
+    organisationId: req.params.organisationId,
+    placement: req.session.data.placement
+  })
+
+  delete req.session.data.placement
+
+  req.flash('success', 'Placement added')
+  res.redirect(`/organisations/${req.params.organisationId}/placements`)
 }
--- app/models/placements.js
+++ app/models/placements.js
@@ -6,23 +6,182 @@ const directoryPath = path.join(__dirname, '../data/placements/')
 
 exports.findMany = (params) => {
     let placements = []
-  
+
     let documents = fs.readdirSync(directoryPath, 'utf8')
 
     // Only get JSON documents
     documents = documents.filter(doc => doc.match(/.*\.(json)/ig))
-  
+
     documents.forEach((filename) => {
       const raw = fs.readFileSync(directoryPath + '/' + filename)
       const data = JSON.parse(raw)
       placements.push(data)
     })
-  
+
     if (params.organisationId) {
         placements = placements.filter(placement => {
           return placement.organisations.find(organisation => organisation.id === params.organisationId)
         })
       }
-  
+
     return placements
   }
+
+// exports.findOne = (params) => {
+//   let placements = []
+
+//   if (params.organisationId) {
+//     placements = this.findMany({ organisationId: params.organisationId })
+//   } else {
+//     placements = this.findMany({})
+//   }
+
+//   let placement = {}
+
+//   if (params.placementId) {
+//     placement = placements.find(placement => placement.id === params.placementId)
+//   }
+
+//   if (parseInt(params.trn)) {
+//     placement = placements.find(placement => placement.trn === parseInt(params.trn))
+//   }
+
+//   return placement
+// }
+
+// exports.saveOne = (params) => {
+//   let placement = {}
+
+//   if (params.organisationId) {
+//     if (params.placementId) {
+//       placement = this.updateOne(params)
+//     } else {
+//       const placementExists = this.findOne({ trn: params.placement.trn })
+
+//       if (placementExists) {
+//         placement = this.updateOne(params)
+//       } else {
+//         placement = this.insertOne(params)
+//       }
+//     }
+//   }
+
+//   return placement
+// }
+
+// exports.insertOne = (params) => {
+//   const placement = {}
+
+//   if (params.organisationId) {
+//     placement.id = uuid()
+
+//     if (params.placement.firstName) {
+//       placement.firstName = params.placement.firstName
+//     }
+
+//     if (params.placement.lastName) {
+//       placement.lastName = params.placement.lastName
+//     }
+
+//     if (params.placement.trn) {
+//       placement.trn = params.placement.trn
+//     }
+
+//     placement.organisations = []
+
+//     const o = organisationModel.findOne({ organisationId: params.organisationId })
+
+//     const organisation = {}
+//     organisation.id = o.id
+//     organisation.name = o.name
+
+//     placement.organisations.push(organisation)
+
+//     placement.createdAt = new Date()
+
+//     const filePath = directoryPath + '/' + placement.id + '.json'
+
+//     // create a JSON sting for the submitted data
+//     const fileData = JSON.stringify(placement)
+
+//     // write the JSON data
+//     fs.writeFileSync(filePath, fileData)
+//   }
+
+//   return placement
+// }
+
+// exports.updateOne = (params) => {
+//   let placement
+//   if (params.placementId) {
+//     placement = this.findOne({ placementId: params.placementId })
+//   } else {
+//     placement = this.findOne({ trn: params.placement.trn })
+//   }
+
+//   if (placement) {
+//     if (params.placement.firstName) {
+//       placement.firstName = params.placement.firstName
+//     }
+
+//     if (params.placement.lastName) {
+//       placement.lastName = params.placement.lastName
+//     }
+
+//     if (params.placement.trn) {
+//       placement.trn = params.placement.trn
+//     }
+
+//     const organisationExists = placement.organisations.find(
+//       organisation => organisation.id === params.organisationId
+//     )
+
+//     if (!organisationExists) {
+//       const o = organisationModel.findOne({ organisationId: params.organisationId })
+
+//       const organisation = {}
+//       organisation.id = o.id
+//       organisation.name = o.name
+
+//       placement.organisations.push(organisation)
+//     }
+
+//     placement.updatedAt = new Date()
+
+//     const filePath = directoryPath + '/' + placement.id + '.json'
+
+//     // create a JSON sting for the submitted data
+//     const fileData = JSON.stringify(placement)
+
+//     // write the JSON data
+//     fs.writeFileSync(filePath, fileData)
+//   }
+
+//   return placement
+// }
+
+// exports.deleteOne = (params) => {
+//   if (params.organisationId && params.placementId) {
+//     const placement = this.findOne({ placementId: params.placementId })
+
+//     placement.organisations = placement.organisations.filter(
+//       organisation => organisation.id !== params.organisationId
+//     )
+
+//     placement.updatedAt = new Date()
+
+//     const filePath = directoryPath + '/' + placement.id + '.json'
+
+//     if (placement.organisations.length) {
+//       // create a JSON sting for the submitted data
+//       const fileData = JSON.stringify(placement)
+//       // write the JSON data
+//       fs.writeFileSync(filePath, fileData)
+//     } else {
+//       // remove the placement altogether since they're no longer associated with an
+//       // organisation
+//       fs.unlinkSync(filePath)
+//     }
+//   }
+// }
+
