e4c10882f6cee2ae78179ffd93825229c15d6d8f
Simon Whatley <simonwhatley@gmail.com>
2024-03-08 09:36:29 +0000

    Add filters - Ofsted, ONS and providers

--- app/helpers/gias.js
+++ app/helpers/gias.js
@@ -394,42 +394,6 @@ exports.getUrbanRuralLabel = (code) => {
   return label
 }
 
-exports.getOfstedRatingOptions = (selectedItem) => {
-  const items = []
-
-  let options = require('../data/dist/ofsted-ratings')
-
-  options.forEach((option, i) => {
-    const item = {}
-
-    item.text = option.name
-    item.value = option.code
-    item.id = option.id
-    item.checked = (selectedItem && selectedItem.includes(option.code)) ? 'checked' : ''
-
-    items.push(item)
-  })
-
-  items.sort((a, b) => {
-    return a.text.localeCompare(b.text)
-  })
-
-  return items
-}
-
-exports.getOfstedRatingLabel = (code) => {
-  const options = require('../data/dist/ofsted-ratings')
-  const option = options.find(option => option.code === code)
-
-  let label = code
-
-  if (option) {
-    label = option.name
-  }
-
-  return label
-}
-
 exports.getSENDProvisionOptions = (selectedItem, noneOption = false) => {
   const items = []
 
---
+++ app/helpers/providers.js
@@ -0,0 +1,22 @@
+const providerModel = require('../models/providers')
+
+exports.getProviderOptions = (params) => {
+  const items = []
+
+
+
+  return items
+}
+
+
+exports.getProviderName = (query) => {
+  const provider = providerModel.findOne({ query })
+
+  let label = query
+
+  if (provider) {
+    label = provider.name
+  }
+
+  return label
+}
