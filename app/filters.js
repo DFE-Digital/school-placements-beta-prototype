cae4934c20bf660094072052aa488a87f2feed13
Simon Whatley <simonwhatley@gmail.com>
2024-03-11 09:54:19 +0000

    Add teacher name filter

--- app/filters.js
+++ app/filters.js
@@ -13,6 +13,7 @@ const onsHelper = require('./helpers/ons')
 const placementHelper = require('./helpers/placements')
 const providerHelper = require('./helpers/providers')
 const subjectHelper = require('./helpers/subjects')
+const teacherHelper = require('./helpers/teachers')
 const utilsHelper = require('./helpers/utils')
 
 /* ------------------------------------------------------------------
@@ -151,6 +152,11 @@ addFilter('getRegionLabel', onsHelper.getRegionLabel)
 Mentor utility functions
 ------------------------------------------------------------------ */
 addFilter('getMentorName', mentorHelper.getMentorName)
+
+/* ------------------------------------------------------------------
+Teacher utility functions
+------------------------------------------------------------------ */
+addFilter('getTeacherName', teacherHelper.getTeacherName)
 
 /* ------------------------------------------------------------------
 Provider utility functions
