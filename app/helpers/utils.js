121b5e4673c27392e3c924dc07e3f734c59576c0
Simon Whatley <simonwhatley@gmail.com>
2024-03-11 18:06:19 +0000

    Move filter functions to separate helper

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -25,25 +25,6 @@ exports.arrayToDateObject = (array) => {
   return new Date(array[2], array[1] - 1, array[0])
 }
 
-exports.getCheckboxValues = (name, data) => {
-  return name && (Array.isArray(name)
-    ? name
-    : [name].filter((name) => {
-        return name !== '_unchecked'
-      })) || data && (Array.isArray(data) ? data : [data])
-}
-
-exports.removeFilter = (value, data) => {
-  // do this check because if coming from overview page for example,
-  // the query/param will be a string value, not an array containing a string
-  if (Array.isArray(data)) {
-    return data.filter(item => item !== value)
-  } else {
-    return null
-  }
-}
-
-
 exports.getSelectedOrganisationTypeItems = (selectedItems, baseHref = '/support/organisations') => {
   const items = []
 
