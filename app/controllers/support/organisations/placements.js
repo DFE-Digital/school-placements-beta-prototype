a4f1c9cf9bc36a183bd9bacb59611078921a8b9a
Simon Whatley <simonwhatley@gmail.com>
2024-03-12 21:20:18 +0000

    Add support organisations placements controller

---
+++ app/controllers/support/organisations/placements.js
@@ -0,0 +1,51 @@
+const organisationModel = require('../../../models/organisations')
+const placementModel = require('../../../models/placements')
+
+const Pagination = require('../../../helpers/pagination')
+
+/// ------------------------------------------------------------------------ ///
+/// LIST PLACEMENT
+/// ------------------------------------------------------------------------ ///
+
+exports.placements_list = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  let placements = placementModel.findMany({ organisationId: req.params.organisationId })
+
+  // TODO: get pageSize from settings
+  let pageSize = 25
+  let pagination = new Pagination(placements, req.query.page, pageSize)
+  placements = pagination.getData()
+
+  res.render('../views/support/organisations/placements/list', {
+    organisation,
+    placements,
+    pagination,
+    actions: {
+      new: '#', //`/organisations/${req.params.organisationId}/placements/new`
+      view: `/organisations/${req.params.organisationId}/placements`,
+      back: '/'
+    }
+  })
+}
+
+/// ------------------------------------------------------------------------ ///
+/// SHOW PLACEMENT
+/// ------------------------------------------------------------------------ ///
+
+exports.placement_details = (req, res) => {
+  const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
+  const placement = placementModel.findOne({
+    organisationId: req.params.organisationId,
+    placementId: req.params.placementId
+  })
+
+  res.render('../views/support/organisations/placements/show', {
+    organisation,
+    placement,
+    actions: {
+      change: `/support/organisations/${req.params.organisationId}/placements/${req.params.placementId}/edit?referrer=change`,
+      delete: `/support/organisations/${req.params.organisationId}/placements/${req.params.placementId}/delete`,
+      back: `/support/organisations/${req.params.organisationId}/placements`
+    }
+  })
+}
