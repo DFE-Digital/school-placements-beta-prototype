456300e9d75e85c47fd34944526c62705dc16f8b
Simon Whatley <simonwhatley@gmail.com>
2024-03-13 15:24:46 +0000

    Add placement name

--- app/decorators/placements.js
+++ app/decorators/placements.js
@@ -1,7 +1,11 @@
 const organisationModel = require('../models/organisations')
 
+const placementHelper = require('../helpers/placements')
+
 exports.decorate = (placement) => {
   const organisation = organisationModel.findOne({ organisationId: placement.organisationId })
+
+  placement.name = placementHelper.getPlacementLabel(placement.subjects)
 
   if (organisation) {
     placement.school = organisation
